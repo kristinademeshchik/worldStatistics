809a7b51d4b79e81fefa3ac0f16bf9d0a7c8037e
Karnitskaya Kristina <kkarnitskaya@marathonbet.ru>
2016-06-01 10:03:46 +0300

    forst commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    path = require('path'),
+    bodyParser = require("body-parser"),
+    exphbs = require('express-handlebars'),
+    app = express();
+
+app.use(express.static('public'));
+
+app.engine('handlebars', exphbs({
+    partialsDir: 'views'
+}));
+
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.render('index', {});
+});
+var server = app.listen(process.env.PORT || 3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
