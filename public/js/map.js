4e51eac4f25c397ebd325d9f15b1cff413514233
Karnitskaya Kristina <kkarnitskaya@marathonbet.ru>
2016-06-07 17:43:34 +0300

    Change hover effect

--- public/js/map.js
+++ public/js/map.js
@@ -7,21 +7,21 @@
 		years = [],
 		defaultColor = '#ccc',
 		svg,
-        chartAreaPath,
-        chartLinePath,
-        chartArea,
-        chartLine,
-        chart,
-        countries,
-        colors,
-        hover,
-        text,
-        select,
-        chartWidth = 300,
-        chartHeight = 200,
-        timeDomain = [],
-        dataDomain = [],
-        margin = {top: 30, right: 40, bottom: 40, left: 40};
+		chartAreaPath,
+		chartLinePath,
+		chartArea,
+		chartLine,
+		chart,
+		countries,
+		colors,
+		hover,
+		text,
+		select,
+		chartWidth = 300,
+		chartHeight = 200,
+		timeDomain = [],
+		dataDomain = [],
+		margin = {top: 30, right: 40, bottom: 40, left: 40};
 
 	function init() {
 		setMap();
@@ -29,277 +29,276 @@
 
 	function setMap() {
 
-        svg = d3.select('#map').append('svg')
-            .attr('width', width)
-            .attr('height', height);
+		svg = d3.select('#map').append('svg')
+			.attr('width', width)
+			.attr('height', height);
 
-        colors = [
-            '#a50026',
-            '#d73027',
-            '#f46d43',
-            '#fdae61',
-            '#fee08b',
-            '#d9ef8b',
-            '#a6d96a',
-            '#66bd63',
-            '#1a9850',
-            '#006837'];
-            
-        defColor = 'white';
-        getColor = d3.scale.quantize()
-            .domain([33, 84])
-            .range(colors);
+		colors = [
+			'#a50026',
+			'#d73027',
+			'#f46d43',
+			'#fdae61',
+			'#fee08b',
+			'#d9ef8b',
+			'#a6d96a',
+			'#66bd63',
+			'#1a9850',
+			'#006837'];
 
-        loadData();
+		defColor = 'white';
+		getColor = d3.scale.quantize()
+			.domain([33, 84])
+			.range(colors);
+
+		loadData();
 	}
 
 	function loadData() {
 		   queue()
-	      .defer(d3.json, '/data/topoworld.json')
-          .defer(d3.csv, '/data/age.csv')
-	      .await(processData); 
+		  .defer(d3.json, '/data/topoworld.json')
+		  .defer(d3.csv, '/data/age.csv')
+		  .await(processData);
 	}
 
-    function processData(error, worldMap, countryData) {
-        var world = topojson.feature(worldMap, worldMap.objects.world),
-            dataValues = [];
+	function processData(error, worldMap, countryData) {
+		var world = topojson.feature(worldMap, worldMap.objects.world),
+			dataValues = [];
 
-        countries = world.features;
+		countries = world.features;
 
-        for (var i in countries) {
-            for (var j in countryData) {
-                if (countries[i].id == countryData[j].ISO3166) {
+		for (var i in countries) {
+			for (var j in countryData) {
+				if (countries[i].id == countryData[j].ISO3166) {
 
-                    for(var k in countryData[j]) {
-                        if (k != 'Country' && k != 'ISO3166' && k !== 'series' && k !== 'data') {
-                            if (years.indexOf(k) == -1) { 
-                                years.push(k);
-                            }
-                            
-                            countries[i].properties[k] = Number(countryData[j][k]);
-                            dataValues.push(Number(countryData[j][k]));
-                        }
-                    }
+					for(var k in countryData[j]) {
+						if (k != 'Country' && k != 'ISO3166' && k !== 'series' && k !== 'data') {
+							if (years.indexOf(k) == -1) {
+								years.push(k);
+							}
 
-                    countries[i].country = countryData[j].Country;
-                    break;
-                }
-            }
-        }
+							countries[i].properties[k] = Number(countryData[j][k]);
+							dataValues.push(Number(countryData[j][k]));
+						}
+					}
 
-        timeDomain = [d3.min(years), d3.max(years)];
-        dataDomain = [d3.min(dataValues), d3.max(dataValues)];
+					countries[i].country = countryData[j].Country;
+					break;
+				}
+			}
+		}
 
-        renderMap(world);
-    }
+		timeDomain = [d3.min(years), d3.max(years)];
+		dataDomain = [d3.min(dataValues), d3.max(dataValues)];
+
+		renderMap(world);
+	}
 
 	function renderMap(world) {
 		var projection,
 			miller = d3.geo.miller()
-	          .scale(130)
-	          .translate([width / 2, height / 2])
-	          .precision(.1);
+			  .scale(130)
+			  .translate([width / 2, height / 2])
+			  .precision(.1);
 
-        projection = d3.geo.path().projection(miller);
+		projection = d3.geo.path().projection(miller);
 
 		var map = svg.append('g');
 
-        map
-            .attr('transform', 'translate(-180, 10)');
+		map
+			.attr('transform', 'translate(-180, 10)');
 
-        select = svg.append('g')
-            .attr('class', 'selected');
+		select = svg.append('g')
+			.attr('class', 'selected');
 
-	    map.selectAll('.country')
-	    	.data(world.features)
-            .enter().append('path')
-            .attr('class', 'country')
-            .attr('d', projection);
+		map.selectAll('.country')
+			.data(world.features)
+			.enter().append('path')
+			.attr('class', 'country')
+			.attr('d', projection);
 
-        map.selectAll('.country')
-        	.on('mouseover', function(d) {
-            	this.style.opacity = '1';
-            })
-            .on('mouseout', function(d) {
-            	this.style.opacity = '';
-            })
-            .on('click', function(d) {
-                setSelected(d, this);
-            });
+		map.selectAll('.country')
+			.on('mouseover', function(d) {
+				this.style.opacity = '1';
+			})
+			.on('mouseout', function(d) {
+				this.style.opacity = '';
+			})
+			.on('click', function(d) {
+				setSelected(d, this);
+			});
 
-        fillMap();
-        setChart();
-        renderHoverData();
-        drawLegend();
+		fillMap();
+		renderChart();
+		renderHoverData();
+		drawLegend();
 	}
 
-    function setSelected(d, context) {
+	function setSelected(d, context) {
 
-        if (!context.classList.contains('country_selected')) {
-            setChartData(d);
-            context.classList.add('country_selected');
-        }
+		if (!context.classList.contains('country_selected')) {
+			setChartData(d);
+			context.classList.add('country_selected');
+		}
 
-        else {
-            context.classList.remove('country_selected');
-            d3.selectAll('.' + d.id).remove();
-        }
-
-    }
-
-	function setChart() {
-        var top,
-            chartX = d3.time.scale()
-            .domain(timeDomain)
-            .range([0, chartWidth]);
-
-        var data = dictToList(countries[1].properties);
-        var chartY = d3.scale.linear()
-            .domain(dataDomain)
-            .range([chartHeight, 0]);
-        
-        var chartXAxis = d3.svg.axis()
-            .scale(chartX)
-            .orient('bottom')
-            .ticks(years.length /2)
-            .tickFormat(d3.format('.0f'));
-
-        var chartYAxis = d3.svg.axis()
-            .scale(chartY)
-            .orient('left')
-            .tickValues(chartY.domain());   
-
-        chartLine = d3.svg.line()
-            .defined(function(d) { return d[1]; })
-            .x(function(d) { return chartX(d[0]); })
-            .y(function(d) { return chartY(d[1]); });
-
-        chartArea = d3.svg.area()
-            .defined(chartLine.defined())
-            .x(chartLine.x())
-            .y0(chartHeight)
-            .y1(chartLine.y());
-
-        chart = svg.append('g');
-
-        chart.attr('transform', 'translate(820, 10)');
-
-        chart.append('rect')
-            .attr('width', chartWidth + 100)
-            .attr('height', chartHeight + 70)
-            .attr('x', 0)
-            .attr('fill', '#fff')
-            .attr('stroke', 'black');
-
-        top = chartHeight + margin.top;
-
-        chart.append('g')
-            .attr('class', 'x axis')
-            .attr('transform', 'translate(' + margin.left + ',' + top + ')')
-            .call(chartXAxis);
-
-        chart.append('g')
-            .attr('class', 'y axis')
-            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-            .call(chartYAxis);
+		else {
+			context.classList.remove('country_selected');
+			d3.selectAll('.' + d.id).remove();
+		}
 
 	}
 
-    function renderHoverData() {
-        hover = svg.append('g')
-            .attr('transform', 'translate(' + (-1000) + ',' + (-1000) + ')');
+	function renderChart() {
+		var top,
+			chartX = d3.time.scale()
+			.domain(timeDomain)
+			.range([0, chartWidth]);
 
-        hover.append('text')
-            .attr('class', 'country-name')
-            .text('Belarus');
+		var data = dictToList(countries[1].properties);
+		var chartY = d3.scale.linear()
+			.domain(dataDomain)
+			.range([chartHeight, 0]);
 
-        hover.append('text')
-            .attr('class', 'year')
-            .text('2000')
-            .attr('transform', 'translate(0, 20)');
+		var chartXAxis = d3.svg.axis()
+			.scale(chartX)
+			.orient('bottom')
+			.ticks(years.length /2)
+			.tickFormat(d3.format('.0f'));
 
-        hover.append('text')
-            .attr('class', 'data')
-            .text('data')
-            .attr('transform', 'translate(0, 40)');
-    }
+		var chartYAxis = d3.svg.axis()
+			.scale(chartY)
+			.orient('left')
+			.tickValues(chartY.domain());
+
+		chartLine = d3.svg.line()
+			.defined(function(d) { return d[1]; })
+			.x(function(d) { return chartX(d[0]); })
+			.y(function(d) { return chartY(d[1]); });
+
+		chartArea = d3.svg.area()
+			.defined(chartLine.defined())
+			.x(chartLine.x())
+			.y0(chartHeight)
+			.y1(chartLine.y());
+
+		chart = svg.append('g');
+
+		chart.attr('transform', 'translate(820, 10)');
+
+		chart.append('rect')
+			.attr('width', chartWidth + 100)
+			.attr('height', chartHeight + 70)
+			.attr('x', 0)
+			.attr('fill', '#fff')
+			.attr('stroke', 'black');
+
+		top = chartHeight + margin.top;
+
+		chart.append('g')
+			.attr('class', 'x axis')
+			.attr('transform', 'translate(' + margin.left + ',' + top + ')')
+			.call(chartXAxis);
+
+		chart.append('g')
+			.attr('class', 'y axis')
+			.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+			.call(chartYAxis);
+
+	}
+
+	function renderHoverData() {
+		hover = svg.append('g')
+			.attr('transform', 'translate(' + (-1000) + ',' + (-1000) + ')');
+
+		hover.append('text')
+			.attr('class', 'country-name')
+			.text('Belarus');
+
+		hover.append('text')
+			.attr('class', 'year')
+			.text('2000')
+			.attr('transform', 'translate(0, 20)');
+
+		hover.append('text')
+			.attr('class', 'data')
+			.text('data')
+			.attr('transform', 'translate(0, 40)');
+	}
 
 
-    function setChartData(country) {
-        var dataset =  country.properties,
-             data = dictToList(dataset);
+	function setChartData(country) {
+		var dataset =  country.properties,
+			 data = dictToList(dataset);
 
-        var xScale = d3.scale.linear()
-            .domain(timeDomain)
-            .range([0, chartWidth]);
+		var xScale = d3.scale.linear()
+			.domain(timeDomain)
+			.range([0, chartWidth]);
 
-        var yScale = d3.scale.linear()
-            .domain(dataDomain)
-            .range([0, chartHeight]);
-
-
-        var chartItem = chart.append('g')
-            .attr('class', country.id);
-
-        chartAreaPath = chartItem.append('path').attr('class', 'area');
-        chartLinePath = chartItem.append('path').attr('class', 'line');
-
-        chartAreaPath
-            .datum(data.filter(function(d) {if (d[1]) return d; }))
-            .attr('d', chartArea)
-            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-        chartLinePath
-            .datum(data.filter(function(d) {if (d[1]) return d; }))
-            .attr('d', chartLine)
-            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+		var yScale = d3.scale.linear()
+			.domain(dataDomain)
+        .range([0, chartHeight]);
 
 
-        var dots = chartItem.append('g')
-            .attr('class', 'dots')
-            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+		var chartItem = chart.append('g')
+			.attr('class', 'country-area ' + country.id);
 
-        dots.selectAll('.dot')
-            .data(data.filter(function(d) {if (d[1]) return d; }))
-            .enter().append('circle')
-            .attr('class', 'dot')
-            .attr('cx', chartLine.x())
-            .attr('cy', chartLine.y())
-            .attr('r', 3)
-            .on('mouseover', function(d) {
-                var xy = d3.mouse(this),
-                    deltaX = 780,
-                    deltaY = 25,
-                    x = (xy[0]) + deltaX,
-                    y = (xy[1] - chartHeight - deltaY);
+		chartAreaPath = chartItem.append('path').attr('class', 'area');
+		chartLinePath = chartItem.append('path').attr('class', 'line');
 
-                if (width - xy[0] < chartWidth) {
-                    x = xy[0] - chartWidth + deltaX;
-                }
-                if (xy[1] < chartHeight) {
-                    y = xy[1] - deltaY;
-                }
+		chartAreaPath
+			.datum(data.filter(function(d) {if (d[1]) return d; }))
+			.attr('d', chartArea)
+			.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-                hover.attr('transform', 'translate(' + x + ',' + y + ')');
-                
-                hover.select('.country-name').text('Country: ' + country.country);
-                hover.select('.year').text('Year: ' + d[0]);
-                hover.select('.data').text('Average years: ' + d[1].toFixed(2));
+		chartLinePath
+			.datum(data.filter(function(d) {if (d[1]) return d; }))
+			.attr('d', chartLine)
+			.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-            })
-            .on('mouseout', function() {
-                hover.attr('transform', 'translate(' + (-1000) + ',' + (-1000) + ')');
-            });
-    }
+		var dots = chartItem.append('g')
+			.attr('class', 'dots')
+			.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-    function dictToList(dict) {
+		dots.selectAll('.dot')
+			.data(data.filter(function(d) {if (d[1]) return d; }))
+			.enter().append('circle')
+			.attr('class', 'dot')
+			.attr('cx', chartLine.x())
+			.attr('cy', chartLine.y())
+			.attr('r', 3)
+			.on('mouseover', function(d) {
+				var xy = d3.mouse(this),
+					deltaX = 780,
+					deltaY = 25,
+					x = (xy[0]) + deltaX,
+					y = (xy[1] - chartHeight - deltaY);
 
-        var list = [];
-        for(var i in dict) {
-            list.push([i, dict[i]]);
-        }
-        return list;
-    }
+				if (width - xy[0] < chartWidth) {
+					x = xy[0] - chartWidth + deltaX;
+				}
+				if (xy[1] < chartHeight) {
+					y = xy[1] - deltaY;
+				}
+
+				hover.attr('transform', 'translate(' + x + ',' + y + ')');
+
+				hover.select('.country-name').text('Country: ' + country.country);
+				hover.select('.year').text('Year: ' + d[0]);
+				hover.select('.data').text('Average years: ' + d[1].toFixed(2));
+
+			})
+			.on('mouseout', function() {
+				hover.attr('transform', 'translate(' + (-1000) + ',' + (-1000) + ')');
+			});
+	}
+
+	function dictToList(dict) {
+
+		var list = [];
+		for(var i in dict) {
+			list.push([i, dict[i]]);
+		}
+		return list;
+	}
 
 	function drawLegend() {
 		var legend = svg.append('g'),
